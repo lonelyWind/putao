a54a7965cda7f00190dd743a4fb712784cd72696
lonelyWind <wxf252577@gmail.com>
2014-03-31 23:38:45 +0800

    header

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -17,4 +17,6 @@ return array(
 		'__CSS__'=>__ROOT__.'/Public/Css',
 		'__JS__'=>__ROOT__.'/Public/Js',
 	),//添加自己的模板变量规则
+	'MODULE_ALLOW_LIST' => array('Home','Admin','User'),	// 允许访问的模块列表
+	'DEFAULT_MODULE'  => 'Home',	// 默认模块
 );
--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -3,9 +3,42 @@
 <head>
 	<meta charset="utf-8">
 	<title>海宁葡萄</title>
-	<include file="Public/header">
+	<link rel="stylesheet" href="/putao/Public/Js/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/putao/Public/Js/dist/css/bootstrap-theme.min.css">
+<script type="text/javascript" src="/putao/Public/Js/jquery.min.js"></script>
+<script type="text/javascript" src="/putao/Public/Js/dist/js/bootstrap.min.js"></script>
 <head>
 <body>
-	<h1><{/putao/Public/Js}></h1>
+	<div class="navbar navbar-default navbar-static-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">海宁葡萄</a>
+			</div>
+			<div class="nav navbar-nav">
+				<ul class="nav navbar-nav">
+					<li class="active">
+						<a href="#">首页</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdon-toggle" data-toggle="dropdown">产品展示<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">葡萄</a></li>
+							<li><a href="#">提子</a></li>
+							<li class="divider"></li>
+							<li class="dropdown-header">灰掉的内容</li>
+							<li><a href="#">亚洲品种</a></li>
+							<li><a href="#">欧美品种</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">关于</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="jumbotron">asdf</div>
+	</div>
 <body>
 <html>
